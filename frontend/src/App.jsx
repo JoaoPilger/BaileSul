81554f17237aeaea0d7bb48beb528c34835b315e
YuriGermano <germanoyuri7@gmail.com>
2026-05-20 23:51:54 -0300

    Realização do frontend da página home, obs: Não está finalizada

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,16 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import Cadastro from './paginas/login_cadastro/cadastro.jsx'
+import Home from './paginas/home/home.jsx'
 import Login from './paginas/login_cadastro/login.jsx'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/cadastro" element={<Cadastro />} />
-        <Route path="/" element={<Navigate to="/login" replace />} />
-        <Route path="*" element={<Navigate to="/login" replace />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   )
